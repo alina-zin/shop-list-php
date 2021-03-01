2f318648697c5355cbfed25cf2867129046bea72
Alina Zinchenko <c0zial00@students.oamk.fi>
2021-03-01 19:24:26 +0200

    poisto ok

--- delete.php
+++ delete.php
@@ -1,3 +1,20 @@
 <?php
 require_once 'inc/header.php';
 require_once 'inc/functions.php';
+
+$input = json_decode(file_get_contents('php://input'));
+$id = filter_var($input->id,FILTER_SANITIZE_NUMBER_INT);
+
+try {
+    $db = openDb();
+
+    $query =$db->prepare('delete from item where id=:id');
+    $query->bindValue(':id',$id,PDO::PARAM_INT);
+    $query->execute();
+
+    $data = array('id' => $id);
+    echo json_encode($data);
+}
+catch (PDOException $pdoex) {
+    returnError($pdoex);
+}
