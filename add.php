06cddaaa3edc7407dc982f8452343d21d99d42bb
Alina Zinchenko <c0zial00@students.oamk.fi>
2021-03-01 19:08:13 +0200

    tiedon lisäys ok

--- add.php
+++ add.php
@@ -1,3 +1,20 @@
 <?php
 require_once 'inc/header.php';
 require_once 'inc/functions.php';
+
+
+$input = json_decode(file_get_contents('php://input'));
+$description = filter_var($input->description,FILTER_SANITIZE_STRING);
+$amount = filter_var($input->amount,FILTER_SANITIZE_NUMBER_INT);
+
+try {
+    $db = openDb();
+    $query = $db->prepare('insert into item(description, amount) value (:description, :amount)');
+    $query->bindValue(':description', $description,PDO::PARAM_STR);
+    $query->bindValue(':amount', $amount,PDO::PARAM_INT);
+    $query->execute();
+    $data = array('id' => $db->lastInsertId(),'description' => $description, 'amount' => $amount);
+    echo json_encode($data);
+} catch (PDOException $pdoex) {
+    returnError($pdoex);
+}
